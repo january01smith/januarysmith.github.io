1b595372f50e75263dda93dc7394aebed02f2cb8
January Smith <january@januarysmith.com>
2016-03-18 08:23:06 -0500

    Add Texas Courthouse Tours page

--- js/blog.js
+++ js/blog.js
@@ -8,21 +8,40 @@
 $(document ).ready(function() {
     $.get( "https://www.googleapis.com/plus/v1/people/108623753953418418615/activities/public?maxResults=100&fields=items(object(attachments%2Fimage%2Furl%2Ccontent%2Curl)%2Cpublished)&key=AIzaSyA3h96Za7Gnx6cCCYZq9qNDZEdpN_jqX-Y", function( response ) {
 
-	var i=0, gObj={},d;
+	buildList(response);
 
-    for(i=0; i<response.items.length; i++){
-    	gObj=response.items[i];
-        d=new Date(gObj.published);
-
-    	$('#podList').append('<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">'+
-    							//'<figure>'+
-    							'<a href="' + gObj.object.url + '" target="_self" title="Picture of the Day \r'+ d.toDateString() +' ">'+
-    								'<img src="' + gObj.object.attachments[0].image.url +'" class="podImage" alt="Picture of the Day '+ d.toDateString() +' ">'+
-    							'</a>'+
-    							//'</figure'+
-    						'</div>');
-    }
+    console.log( "Load was performed: " + response);
 
 	});
 });
 
+function loadMore(nextPageToken){
+    $.get( "https://www.googleapis.com/plus/v1/people/108623753953418418615/activities/public?maxResults=100&fields=items(object(attachments%2Fimage%2Furl%2Ccontent%2Curl)%2Cpublished)&pageToken="+ nextPageToken + "&key=AIzaSyA3h96Za7Gnx6cCCYZq9qNDZEdpN_jqX-Y", function( response ) {
+
+    buildList(response);
+
+    });
+}
+
+function buildList(response){
+
+    var i=0, gObj={},podDate;
+
+    for(i=0; i<response.items.length; i++){
+        gObj=response.items[i];
+        podDate=new Date(gObj.published);
+
+        $('#podList').append('<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">'+
+                                //'<figure>'+
+                                '<a href="' + gObj.object.url + '" target="_self" title="Picture of the Day \r'+ podDate.toDateString() +' ">'+
+                                    '<img src="' + gObj.object.attachments[0].image.url +'" class="podImage" alt="Picture of the Day '+ podDate.toDateString() +' ">'+
+                                '</a>'+
+                                //'</figure'+
+                            '</div>');
+    }
+
+    // if (response.nextPageToken!='') {
+    //     $('#podList').append('<button type="button" class="btn" onclick="loadMore(' + response.nextPageToken + ')">Load More</button>');
+    //     console.log(response.nextPageToken);
+    //}
+}
